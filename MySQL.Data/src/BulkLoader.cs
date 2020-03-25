5c7b9c71e2ccdcf3352fd365fced2edd27398819
yiting2002 <yiting2002@gmail.com>
2020-03-25 17:13:17 +0800

    修正檔案編碼

--- MySQL.Data/src/BulkLoader.cs
+++ MySQL.Data/src/BulkLoader.cs
@@ -1,4 +1,4 @@
-// Copyright � 2006, 2019, Oracle and/or its affiliates. All rights reserved.
+﻿// Copyright © 2006, 2019, Oracle and/or its affiliates. All rights reserved.
 //
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License, version 2.0, as
